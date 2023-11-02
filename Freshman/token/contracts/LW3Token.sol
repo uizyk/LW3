0ccee2adec7b5e934af9fa0a9cfc49f3291324a9
Sam <uizykim@gmail.com>
2023-11-02 17:42:21 +0900

    freshman LW3Token progress

---
+++ Freshman/token/contracts/LW3Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: M
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LW3Token is ERC20 {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _mint(msg.sender, 10 * 10 ** 18);
+    }
+}
